9dff1dce3ad170beb199e4b60e03b57b2a6768cb
Aleksander Laurowski <ext-a.laurowski@omikron.net>
2020-05-19 08:25:32 +0200

    Added export attributes(properties)

--- Components/Data/Article/Fields/Attributes.php
+++ Components/Data/Article/Fields/Attributes.php
@@ -4,18 +4,37 @@ declare(strict_types=1);
 
 namespace OmikronFactfinder\Components\Data\Article\Fields;
 
+use OmikronFactfinder\Components\Filter\TextFilter;
 use Shopware\Models\Article\Article;
+use Shopware\Models\Property\Value;
 
 class Attributes implements ArticleFieldInterface
 {
+    /** @var TextFilter */
+    private $filter;
+
+    public function __construct(TextFilter $filter)
+    {
+        $this->filter = $filter;
+    }
+
     public function getName(): string
     {
-       return 'Attributes';
+        return 'Attributes';
     }
 
     public function getValue(Article $article): string
     {
-       $properties = $article->getPropertyGroup();
-       //@todo implement;
+        $attributes = array_reduce($article->getPropertyValues()->toArray(), function (array $attributes, Value $value) {
+            $attributes[] = $this->formatAttribute($value->getOption()->getName(), $value->getValue());
+            return $attributes;
+        }, []);
+
+        return $attributes ? '|' . implode('|', $attributes) . '|' : '';
+    }
+
+    private function formatAttribute($name, $value)
+    {
+        return "{$this->filter->filterValue($name)}={$this->filter->filterValue($value)}";
     }
 }
