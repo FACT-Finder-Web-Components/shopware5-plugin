52c06a3de510ab8dc70b1de9a13dbfcb95083f80
Francesco Marangi <2734054+fmarangi@users.noreply.github.com>
2020-09-08 11:41:36 +0200

    Ensure compatibility with Shopware v5.5

---
+++ BackwardCompatibility/BackwardCompatibilityCompilerPass.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OmikronFactfinder\BackwardCompatibility;
+
+use OmikronFactfinder\Components\Data\Article\Articles;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
+
+class BackwardCompatibilityCompilerPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+        if ($container->has('shopware.components.shop_registration_service')) {
+            return; // On Shopware 5.6, proceed...
+        }
+
+        eval('namespace Shopware\Components{interface ShopRegistrationServiceInterface{}}');
+        $shopRegistration = new Definition(ShopRegistrationService::class, [new Reference('service_container')]);
+        $container->setDefinition('shopware.components.shop_registration_service', $shopRegistration);
+
+        if ($container->has(Articles::class)) {
+            $articleResource = new Definition(ArticleResource::class, [new Reference('translation')]);
+            $articleResource->addMethodCall('setManager', [new Reference('models')]);
+            $container->getDefinition(Articles::class)->setArgument('$articleResource', $articleResource);
+        }
+    }
+}
--- OmikronFactfinder.php
+++ OmikronFactfinder.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace OmikronFactfinder;
 
+use OmikronFactfinder\BackwardCompatibility\BackwardCompatibilityCompilerPass;
 use OmikronFactfinder\Components\Data\Article\Fields\ArticleFieldInterface;
 use Shopware\Components\Plugin;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -13,6 +14,7 @@ class OmikronFactfinder extends Plugin
     public function build(ContainerBuilder $container): void
     {
         parent::build($container);
+        $container->addCompilerPass(new BackwardCompatibilityCompilerPass());
         $container->registerForAutoconfiguration(ArticleFieldInterface::class)->addTag('factfinder.export.field');
     }
 }
