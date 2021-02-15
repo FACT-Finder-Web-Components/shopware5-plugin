09e2f036311500ff5359d9e6a4bf5e45728644ee
Aleksander Laurowski <ext-a.laurowski@omikron.net>
2021-02-15 07:23:20 +0100

    fixup! FFWEB-1940 export localized data

--- Components/Data/Article/PriceCurrencyFields.php
+++ Components/Data/Article/PriceCurrencyFields.php
@@ -6,6 +6,7 @@ namespace OmikronFactfinder\Components\Data\Article;
 
 use OmikronFactfinder\Components\Data\Article\Fields\PriceCurrency;
 use OmikronFactfinder\Components\Formatter\NumberFormatter;
+use Shopware\Components\Model\ModelManager;
 use Shopware\Models\Shop\Currency;
 
 class PriceCurrencyFields
@@ -13,14 +14,19 @@ class PriceCurrencyFields
     /** @var NumberFormatter */
     private $numberFormatter;
 
+    /** @var ModelManager */
+    private $modelManager;
+
     /** @var array */
     private $fieldRoles;
 
     public function __construct(
         NumberFormatter $numberFormatter,
+        ModelManager $modelManager,
         array $fieldRoles
     ) {
         $this->numberFormatter = $numberFormatter;
+        $this->modelManager    = $modelManager;
         $this->fieldRoles      = $fieldRoles;
     }
 
@@ -29,6 +35,6 @@ class PriceCurrencyFields
         return array_merge([], ...array_map(function (Currency $currency): array {
             $colName = sprintf('%s_%s', $this->fieldRoles['price'], $currency->getName());
             return [$colName => new PriceCurrency($this->numberFormatter, $currency, $colName)];
-        }, Shopware()->Models()->getRepository(Currency::class)->findAll()));
+        }, $this->modelManager->getRepository(Currency::class)->findAll()));
     }
 }
