4d066fadc4bb7f1ccd721f772d9b5194398bd150
Aleksander Laurowski <ext-a.laurowski@fact-finder.com>
2021-12-22 09:46:44 +0100

    do not use sid generated from real session id

--- Controllers/Frontend/Factfinder.php
+++ Controllers/Frontend/Factfinder.php
@@ -13,7 +13,6 @@ class Shopware_Controllers_Frontend_Factfinder extends \Enlight_Controller_Actio
         $view = $this->View();
         $view->assign('addParams', $this->Request()->getParam('addParams', ''));
         $view->assign('searchImmediate', $this->Request()->getParam('searchImmediate', false));
-        $view->assign('sid', $this->get('session')->get('sessionId'));
         $view->assign('uid', (int) $this->get('session')->get('sUserId'));
     }
 }
