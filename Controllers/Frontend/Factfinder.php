e24a2782a57c41e8df7fb6edd5bd5b0018a58e32
Grzegorz Jamróz <grzegorz.jamrooz@gmail.com>
2022-10-24 10:00:28 +0200

    FFWEB-2576: Anonymize `user-id` (#73)

--- Controllers/Frontend/Factfinder.php
+++ Controllers/Frontend/Factfinder.php
@@ -14,6 +14,14 @@ class Shopware_Controllers_Frontend_Factfinder extends \Enlight_Controller_Actio
         $view->assign('addParams', $this->Request()->getParam('addParams', ''));
         $view->assign('categoryPage', $this->Request()->getParam('categoryPage', ''));
         $view->assign('searchImmediate', $this->Request()->getParam('searchImmediate', false));
-        $view->assign('uid', (int) $this->get('session')->get('sUserId'));
+        $view->assign('uid', $this->getUserId());
+    }
+
+    protected function getUserId(): string
+    {
+        $configuration = $this->get('OmikronFactfinder\Components\Configuration');
+        $userId        = (string) $this->get('session')->get('sUserId');
+
+        return $configuration->isFeatureEnabled('ffAnonymizeUserId') ? md5($userId) : $userId;
     }
 }
