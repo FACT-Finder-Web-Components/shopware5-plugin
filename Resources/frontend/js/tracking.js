a49469a9504a12f305367b20bf78ad5ae81f2d7f
Grzegorz Jamróz <grzegorz.jamrooz@gmail.com>
2022-12-27 14:31:42 +0100

    FFWEB-2645: new cart tracking

--- Resources/frontend/js/tracking.js
+++ Resources/frontend/js/tracking.js
@@ -1,15 +1,43 @@
 ;(function ($) {
-    $.subscribe('plugin/swAddArticle/onBeforeAddArticle', function (ev, me, data) {
+    function getRecordId(data)
+    {
         const params = new RegExp('[\?&]sAdd=([^&#]*)').exec(data);
 
         if (!params || !params[1]) {
+            return '';
+        }
+
+        return params[1];
+    }
+
+    function getQuantity(data)
+    {
+        if (ffTrackingSettings.addToCart.count === 'count_as_one') {
+            return 1;
+        }
+
+        const params = new RegExp('[\?&]sQuantity=([^&#]*)').exec(data);
+
+        if (!params || !params[1]) {
+            return 1;
+        }
+
+        return parseInt(params[1]);
+    }
+
+    $.subscribe('plugin/swAddArticle/onBeforeAddArticle', function (ev, me, data) {
+        const recordId = getRecordId(data);
+        const quantity = getQuantity(data);
+
+        if (recordId === '') {
             return;
         }
+
         waitForFactFinder().then(function (factfinder) {
             const trackingHelper = factfinder.communication.Util.trackingHelper;
             factfinder.communication.EventAggregator.addFFEvent({
                 type: 'getRecords',
-                recordId: params[1],
+                recordId: recordId,
                 idType: 'productNumber',
                 success: function (response) {
                     if (!response || !response[0]) {
@@ -20,7 +48,7 @@
                         id: trackingHelper.getTrackingProductId(product),
                         masterId: trackingHelper.getMasterArticleNumber(product),
                         price: factfinder.communication.Util.trackingHelper.getPrice(product),
-                        count: 1,
+                        count: quantity,
                     });
                 },
             });
